4dd7c2d622a1a034b6aa6f3a8d6c6e9c95025a6a
mendozaolives <oliverclintmendoza@gmail.com>
2022-04-17 23:05:53 -0400

    Added data_out to RAM

--- rtl/64x8Ram.vhd
+++ rtl/64x8Ram.vhd
@@ -2,18 +2,18 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity 4x8Ram is port(
-address : in std_logic_vector(1 downto 0);
-data_in : in std_logic_vector(1 downto 0);
+entity 64x8Ram is port(
+address : in std_logic_vector(15 downto 0);
+data_in : in std_logic_vector(7 downto 0);
 write_in : in std_logic;
 clock : in std_logic;
 data_out : out std_logic_vector(7 downto 0);
 
 );
-end 4x8Ram;
+end 64x8Ram;
 
-architecture sim of 4x8Ram is
-    type RamArray is array (0 to 3) of std_logic_vector(7 downto 0);
+architecture rtl of 64x8Ram is
+    type RamArray is array (0 to 63) of std_logic_vector(7 downto 0);
 
     signal RamData: RamArray :=(
 
@@ -28,4 +28,6 @@ begin
         end if
     end if
 end process
+
+    data_out <= RamData(to_integer(unsigned(address)));
 end architecture;

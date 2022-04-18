51f6bce041dc1c4e5aa19994d8a3be761e67b1ab
mendozaolives <oliverclintmendoza@gmail.com>
2022-04-18 17:42:35 -0400

    Adjustments

--- rtl/64x8Ram.vhd
+++ rtl/64x8Ram.vhd
@@ -4,16 +4,16 @@ use ieee.numeric_std.all;
 
 entity 64x8Ram is port(
 address : in std_logic_vector(15 downto 0);
-data_in : in std_logic_vector(7 downto 0);
 write_in : in std_logic;
 clock : in std_logic;
+data_in : in std_logic_vector(7 downto 0);
 data_out : out std_logic_vector(7 downto 0);
 
 );
 end 64x8Ram;
 
 architecture rtl of 64x8Ram is
-    type RamArray is array (0 to 63) of std_logic_vector(7 downto 0);
+    type RamArray is array (0 to 63*1024) of std_logic_vector(7 downto 0);
 
     signal RamData: RamArray :=(
 

7a35df3ccb31c6bc76dc199b32952cc57a976bc5
forgottenecho <39808231+forgottenecho@users.noreply.github.com>
2022-04-30 14:17:33 -0400

    LDAC, program segment 2

--- rtl/MainMem.vhd
+++ rtl/MainMem.vhd
@@ -25,6 +25,8 @@ begin
     -- initialize memory
     if is_start = '1' then
         is_start <= '0';
+		
+		-- INSTRUCTIONS
         RamData(0) <= x"0A"; -- INAC
         RamData(1) <= x"00"; -- NOP
         RamData(2) <= x"06"; -- JMPZ
@@ -34,6 +36,25 @@ begin
         RamData(6) <= x"05"; -- JMP 1000
         RamData(7) <= x"01"; -- GAMMA LOW
         RamData(8) <= x"00"; -- GAMMA HIGH
+
+        RamData(4659) <= x"01"; -- LDAC
+        RamData(4660) <= x"EF"; -- GAMMA LOW
+        RamData(4661) <= x"BE"; -- GAMMA HIGH
+        RamData(4662) <= x"03"; -- MVAC
+        RamData(4663) <= x"01"; -- LDAC
+        RamData(4664) <= x"AD"; -- GAMMA LOW
+        RamData(4665) <= x"DE"; -- GAMMA HIGH
+        RamData(4666) <= x"0D"; -- OR
+        RamData(4667) <= x"0E"; -- XOR
+        RamData(4668) <= x"09"; -- SUB
+
+
+		-- DATA
+        RamData(47806) <= x"00"; -- JMP 1000
+        RamData(48879) <= x"AA"; -- GAMMA LOW
+        RamData(57005) <= x"F0"; -- GAMMA HIGH
+		
+		
 
 
 
